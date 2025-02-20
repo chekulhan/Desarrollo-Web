68dde5c8d4b4ad37102a8bfe4fec480bc9b69d0f
Che Kulhan <che.kulhan@gmail.com>
2025-02-20 22:30:55 +0100

    querystring

---
+++ UF1841-Web-Front-End/nodejs/5-node.querystring.js
@@ -0,0 +1,27 @@
+const http = require('http');
+const url = require('url');
+
+const server = http.createServer((req, res) => {
+    if (req.method === 'GET') {
+        // Parse the URL
+        const parsedUrl = url.parse(req.url, true); // The second argument `true` parses the query string
+        const queryData = parsedUrl.query; // Access the query object
+
+        // Example: Get specific query parameters
+        const name = queryData.name; // Assuming the URL is ?name=John
+        const age = queryData.age;   // Assuming the URL is ?age=30
+
+        // Respond with the query data
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end(`Name: ${name}, Age: ${age}`);
+    } else {
+        res.writeHead(405, { 'Content-Type': 'text/plain' });
+        res.end('Method Not Allowed');
+    }
+});
+
+const hostname = '127.0.0.1';
+const port = 3000;
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
