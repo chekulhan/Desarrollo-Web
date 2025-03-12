46d7354fa5d2d9d2e8278eb7e284d16c36cd2d5e
Che Kulhan <che.kulhan@gmail.com>
2025-03-12 13:43:11 +0100

    functions

--- UF1842-Web-Components/javascript/06-functions.recursividad.js
+++ UF1842-Web-Components/javascript/06-functions.recursividad.js
@@ -15,6 +15,15 @@ function sumar(a, b) {
     return a + b;
 }
 
+// Actividad de REPASO (básico)
+// 1. Crear una funcion para calcular el área de un rectángulo. Por ejemplo area(3, 4) = 12
+// 2. Crear la función para repetir x veces:
+//      const mensaje = repeatMessage("Hola! ", 3));
+//      console.log(mensaje); 
+//      Output: "Hola! Hola! Hola!"
+
+
+// ARROW FUNCTIONS
 // Usar const con arrow functions (anónimos) o function keyword:
 // const con funciones flecha: Las funciones asignadas a const no se elevan, lo que significa que debes definirlas antes de usarlas.
 // no tienen su propio 'this'
@@ -40,16 +49,29 @@ function saludo(nombre, edad) {
 
 let notas = [2, 5, 3, 2, 9, 8];
 
-
+// PASS by VALUE (inmutable) y PASS by REF (mutable):
 // Recordar que si Pass-by-Reference un Objeto, se modifica su valor original
-function cambiarNombre(obj) {
-    obj.nombre = "Jon"; // Modificar el objeto original
-  }
-  
-let persona = { nombre: "Jane" };
-cambiarNombre(persona);
-console.log(persona.nombre); // Output: "Jon" (The original object was modified)
-  
+// Actividad 1: Primitive
+
+let val = 10;
+
+function modificarVal(x) {
+  x = x * 2; 
+  console.log("Dentro de la funcion:", x);
+}
+
+console.log(val); // ¿Cual es el valor de val?
+
+// Actividad 2: Objeto
+let val = {width: 5};
+
+function modificarObject(obj) {
+  obj.width = obj.width * 2; 
+  console.log("Dentro de la funcion:", obj); 
+}
+
+console.log(val); // ¿Cual es el valor de val?
+
 
 // Recursividad -------------------------------
 function factorial(n) {
@@ -82,10 +104,22 @@ setTimeout(() => {
     countDown(number - 1);
 }, 1000); // Delay 1 second between counts
 
+// Actividad de repaso (avanzado)
+// Crear una funcion recursiva para sumar los numeros de cada segundo elemento.
+// Por ejemplo, sumarSegundo(7) = 7+5+3+1 = 16
+// sumarSegundo(6) = 6+4+2 = 12
 
 
 // --------------------------------
 // Respuestas:
+
+function sumSecondNumbers(n) {
+  if (n <= 0) return 0; // Base case: Stop when n reaches 0 or negative
+  return n + sumSecondNumbers(n - 2); // Recursive step: Add n and call function with n-2
+}
+
+
+
 const saludo = (nombre, edad) => {
     if (edad >= 18) {
       return `Hola, ${nombre}. Eres mayor de edad.`;
